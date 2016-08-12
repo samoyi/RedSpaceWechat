15abbd98a9adb39494a1c449d308cfdb414973e5
FuncaIDT <info@funca.com.cn>
2016-08-12 18:51:41 +0800

    delete RedSpace Wechat configration data

--- configration.php
+++ configration.php
@@ -4,9 +4,9 @@
  *
  */
 
-define("TOKEN", "RedSpace");
-define("APPID", "wx2e87611162aae7f4");
-define("APPSECRET", "9aef7e06d6c0cf735762e2982fa755ca");
+define("TOKEN", "");
+define("APPID", "");
+define("APPSECRET", "");
 
 
 ?>
