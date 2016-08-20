425280071b632cad58ccdc7ed88fb94ecf3c7002
FuncaIDT <info@funca.com.cn>
2016-08-20 18:53:16 +0800

    none

--- index.php
+++ index.php
@@ -21,16 +21,6 @@ include('WechatPushed.php'); // 获取微信后台推送信息
 //每次设置完后关闭，否则只要运行该文件就会重复设置
 //include('module/setCustomMenuData.php');
 
-//上传图片测试区域
-include('class/MaterialManager.class.php');    
-$MaterialManager = new MaterialManager();  
-$image_info = array(
-    'filename'=>'/images/1.png',  //国片相对于网站根目录的路径
-    'content-type'=>'image/png',  //文件类型
-    'filelength'=>'156023'         //图文大小
-);                                  
-file_put_contents("err.txt", $MaterialManager->addImage($image_info) );
-//上传图片测试区域结束
 
 
 //关键词自动回复和事件推送回复
