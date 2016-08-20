c0196ca96ed61a46dbcb3f431944761429bdb18c
FuncaIDT <info@funca.com.cn>
2016-08-20 19:43:00 +0800

    加入上传图片函数

--- class/MaterialManager.class.php
+++ class/MaterialManager.class.php
@@ -3,15 +3,19 @@
 class MaterialManager
 {
 	    
-    public function addImage($image_info)
+    public function addImage()
 	{
 		$type = "image";
-		$filepath = dirname(__FILE__)."\winter.jpg";
+		$filepath = dirname(dirname(__FILE__))."/image/33.png";
 		$filedata = array("media"  => "@".$filepath);
-		$url = "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token=$access_token&type=$type";
-		$result = https_request($url, $filedata); 
-		var_dump($result); 
-		function https_request($url, $data = null)
+		//$url = "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token=" . ACCESS_TOKEN . "&type=$type";
+		// 应该是这个url？ https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=ACCESS_TOKEN
+		$url = "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=" . ACCESS_TOKEN . "&type=$type";
+
+		$result = request_post($url, $filedata); 
+		echo $result; 
+		// id   wptdc2AEc7V_tFYzTD1EMbeiIBESEpEzAUJgCcG_A9o
+		/*function request_post($url, $data = null)
 		{
 		    $curl = curl_init();
 		    curl_setopt($curl, CURLOPT_URL, $url);
@@ -25,7 +29,7 @@ class MaterialManager
 		    $output = curl_exec($curl);
 		    curl_close($curl);
 		    return $output;
-		}
+		}*/
 		//似乎一直在上传还是在干什么，超过5分钟也不成功
 	    /*$url = "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=" . ACCESS_TOKEN . "&type=image";
 	    file_put_contents("err.txt", $url . "<br />", FILE_APPEND);
@@ -53,6 +57,38 @@ class MaterialManager
 	    {
 	        return false;
 	    }*/
+
+
+
+
+//上传图片
+/*$type = "image";
+$filepath = dirname(__FILE__)."\winter.jpg";
+$filedata = array("media"  => "@".$filepath);
+$url = "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token=$access_token&type=$type";
+$result = https_request($url, $filedata); 
+var_dump($result); 
+function https_request($url, $data = null)
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+    if (!empty($data)){
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    }
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($curl);
+    curl_close($curl);
+    return $output;
+}
+*/
+
+
+
+
+
 	}
 }
 
--- test.php
+++ test.php
@@ -13,12 +13,12 @@ include('WechatPushed.php'); // 获取微信后台推送信息
 
 include('class/MaterialManager.class.php');    
 $MaterialManager = new MaterialManager();  
-$image_info = array(
+/*$image_info = array(
     'filename'=>'/images/1.png',  //国片相对于网站根目录的路径
     'content-type'=>'image/png',  //文件类型
     'filelength'=>'156023'         //图文大小
-);                                  
-$MaterialManager->addImage($image_info);
+); */                                 
+$MaterialManager->addImage();
 
 
 
