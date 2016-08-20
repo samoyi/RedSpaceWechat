5d3231ffd02d9b1bf58363095f53e80b51161d99
FuncaIDT <info@funca.com.cn>
2016-08-20 18:21:57 +0800

    none

--- class/MaterialManager.class.php
+++ class/MaterialManager.class.php
@@ -3,9 +3,32 @@
 class MaterialManager
 {
 	    
-    function addImage($image_info)
+    public function addImage($image_info)
 	{
-	    $url="https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=" . ACCESS_TOKEN . "&type=image";
+		$type = "image";
+		$filepath = dirname(__FILE__)."\winter.jpg";
+		$filedata = array("media"  => "@".$filepath);
+		$url = "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token=$access_token&type=$type";
+		$result = https_request($url, $filedata); 
+		var_dump($result); 
+		function https_request($url, $data = null)
+		{
+		    $curl = curl_init();
+		    curl_setopt($curl, CURLOPT_URL, $url);
+		    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+		    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+		    if (!empty($data)){
+		        curl_setopt($curl, CURLOPT_POST, 1);
+		        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+		    }
+		    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		    $output = curl_exec($curl);
+		    curl_close($curl);
+		    return $output;
+		}
+		//似乎一直在上传还是在干什么，超过5分钟也不成功
+	    /*$url = "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=" . ACCESS_TOKEN . "&type=image";
+	    file_put_contents("err.txt", $url . "<br />", FILE_APPEND);
 	    $ch1 = curl_init ();
 	    $timeout = 5;
 	    $real_path="{$_SERVER['DOCUMENT_ROOT']}{$file_info['filename']}";
@@ -29,7 +52,7 @@ class MaterialManager
 	    else 
 	    {
 	        return false;
-	    }
+	    }*/
 	}
 }
 
