77802b8b19cb1c55595d01856af06416784a0417
FuncaIDT <info@funca.com.cn>
2016-11-12 11:30:45 +0800

    在decoupling分支中也加入订单邮件提醒

--- module/eventAutoReply.php
+++ module/eventAutoReply.php
@@ -141,6 +141,13 @@ switch( EVENT_TYPE )
 		$UserManager = new UserManager();
 		$UserManager->noteUseOrderInfo($orderDetail);
 
+		// 邮件提醒
+		require "plugin/sendmail.php";
+		$mail_to = '601610407@qq.com';//接收人邮箱
+		$mail_subject = '红房子微信小店新订单提醒';//邮件标题
+		$mail_message = '红房子微信小店新订单提醒';//邮件内容
+		sendmail($mail_to, $mail_subject, $mail_message);
+		
         // 发卡券
         /* include('class/CardMessager.class.php');
         $cardMessager = new CardMessager();
