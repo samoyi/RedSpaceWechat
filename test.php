e0d88edee64b419cd6d3f51926e5ad872b58d38e
FuncaIDT <info@funca.com.cn>
2016-11-12 11:29:06 +0800

    none

--- test.php
+++ test.php
@@ -18,10 +18,10 @@ require PROJECT_ROOT . 'class/MySQLiController.class.php';
 //$MySQLiController = new MySQLiController( $dbr );
 
 
-
-$query = 'UPDATE Wechat_OpenID SET receiver_name="小红红",receiver_province="河南省",receiver_city="信阳市",receiver_zone="浉河区",receiver_address="申城大道，6月9号送达，蛋糕上请写:祝宝贝生日快乐！" WHERE openID="okV_gjrMpNfy6d5fJxqj7ph68MmU"';
-$dbr->query( $query );
-//$MySQLiController->changeColumnType(DB_TABLE, 'isSubscribing', 'INT UNSIGNED NOT NULL');
+require "class/ProductManager.class.php";
+$ProductManager = new ProductManager();
+$result = $ProductManager->queryProductIDs(1);
+print_r( $result );
 
 //关键词自动回复和事件推送回复
 //该部分最后会跳出程序，下面这行应该是放在最后
