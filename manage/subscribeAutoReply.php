371b3d975def2be72599688cbf12d92ecf58289c
samoyi <greatlining@163.com>
2017-03-04 14:23:58 +0800

    管理页面添加了编辑新关注自动回复的功能

---
+++ manage/subscribeAutoReply.php
@@ -0,0 +1,31 @@
+<?php
+//date_default_timezone_set("Asia/Hong_Kong");
+	
+	if( isset($_GET['command']) )
+	{
+		switch( $_GET['command'] )
+		{
+			case 'copySubscribeAutoReplyJSON':
+			{
+				if( copy('JSONData/subscribeAutoPlayText.json', 'JSONData/subscribeAutoPlayText_copy.json') )
+				{
+					echo 'success';
+				}
+				break;
+			}
+		}
+	}
+	
+	if( isset($_POST['newJSON']) )
+	{
+		$sNewJSON = stripslashes(urldecode($_POST['newJSON']));
+		if( file_put_contents('JSONData/subscribeAutoPlayText.json', $sNewJSON) )
+		{
+			echo 'success';
+		}
+	}
+	
+	
+	
+	
+?>
